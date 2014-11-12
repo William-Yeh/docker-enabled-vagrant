0c1d33e184ae143bc533bd159cbc9fe30a05b3e6
William Yeh <william.pjyeh@gmail.com>
2014-11-12 12:13:35 +0800

    fix: missing 'tar' commands

--- ubuntu-trusty/vagrant-provision.sh
+++ ubuntu-trusty/vagrant-provision.sh
@@ -81,7 +81,7 @@ rm *.tar.gz
 # install dockerize
 # @see https://github.com/jwilder/dockerize
 curl -o dockerize.tar.gz -L https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/$DOCKERIZE_TARBALL
-sudo -xzvf dockerize.tar.gz
+tar xzvf dockerize.tar.gz
 sudo chown root dockerize
 sudo chgrp root dockerize
 sudo mv dockerize /usr/local/bin
